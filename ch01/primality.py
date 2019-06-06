7a7970c7906715e64a5c25e2df67d483f3cb14f5
hexu <hexu_1985@sina.com>
2019-06-06 16:59:31 +0800

    add primality.py

---
+++ ch01/primality.py
@@ -0,0 +1,27 @@
+import random
+import time
+import modexp
+
+def primality(N):
+    '''
+    Input: Positive integer N
+    Output: yes/no
+    '''
+    # Pick a positive integer a < N at random
+    random.seed(time.time())
+    a = random.randint(1, N-1)
+    if modexp.modexp(a, N-1, N) == 1:
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    def test_primality(N):
+        if primality(N):
+            print("%d is pass primality test" % N)
+        else:
+            print("%d is not pass primality test" % N)
+
+    test_primality(341)
+    test_primality(499)
+
